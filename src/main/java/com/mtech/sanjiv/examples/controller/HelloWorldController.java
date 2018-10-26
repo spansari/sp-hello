07a0ddbc0d4a01fc80f75c0c0633597591c419d7
Sanjiv Pansari <sanjiv.pansari@macys.com>
2018-10-25 22:47:50 -0400

    initial commit

---
+++ src/main/java/com/mtech/sanjiv/examples/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.mtech.sanjiv.examples.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+   @RequestMapping("/hello")
+   public String sayHello() {
+      return "Hello from Service Version 2.22222222222222222222222222222222";
+   }
+}
