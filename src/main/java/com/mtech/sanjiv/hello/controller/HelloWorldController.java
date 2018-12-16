a93e6e49c2da574d181356a674fecf2d079830f3
Sanjiv Pansari <sanjiv.pansari@macys.com>
2018-12-15 21:11:32 -0500

    spring cloud config to get properties from config server

--- src/main/java/com/mtech/sanjiv/hello/controller/HelloWorldController.java
+++ src/main/java/com/mtech/sanjiv/hello/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.mtech.sanjiv.hello.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
+@RefreshScope
 public class HelloWorldController {
 
 
-    @Value("${spring.datasource.username}")
-    private String dbUser;
+    @Value("${app.greet.name}")
+    private String name;
 
     @RequestMapping("/hello")
    public String sayHello() {
-       return "Hello from Service Version 2.22222222 : "+dbUser;
+       return "Hello from Service Version 2.22222222 : "+name;
    }
 
    @PostMapping("msg")
